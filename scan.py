e4a54fe35ae82c614e2c363cc76d39cfc3aab055
redhorizon0309 <redhorizon0309@gmail.com>
2021-09-27 05:52:40 +0700

    [Update]Scan AP file handler

--- scan.py
+++ scan.py
@@ -1,61 +1,79 @@
-
 #!/usr/bin/env python
 
 import sys, os, signal, re
+import xml.etree.ElementTree as ET
 from time import sleep
 from multiprocessing import Process
 from scapy.all import *
 
-interface='' # monitor interface
+aps_list = 'APs.xml'
+interface = '' # monitor interface
 aps = {} # dictionary to store unique APs
 
 def monitor_up(interface):
-    os.system("sudo ifconfig %s down" % (str(interface)))
-    os.system("sudo iwconfig %s mode monitor" % (str(interface)))
-    os.system("sudo ifconfig %s up" % (str(interface)))
+    os.system("sudo airmon-ng start %s" % (str(interface)))
+    interface = str(interface+'mon') 
+    return interface
 
-def monitor_down(i):
-    os.system("sudo ifconfig %s down" % (str(interface)))
-    os.system("sudo iwconfig %s mode manager" % (str(interface)))
-    os.system("sudo ifconfig %s up" % (str(interface)))
+def monitor_down(interface):
+    #print("turn %s to station mode" % (str(interface)))
+    os.system("sudo airmon-ng stop %s" % (str(interface)))
 
-def printAP():
-    with open('APs.txt','r') as aps_list:
-        for line in aps_list:
-            print(line,end = '')
+#AP handler
+def padding(length, text):
+    for i in range(0,length):
+        if text[i] == "" :
+            text = text+" "
+             
+def APs_init(fileName):
+    aps = ET.Element('AccessPoints')
+    apsList = ET.ElementTree(aps)
+    with open (fileName, "wb") as files :
+        apsList.write(files)
 
-def isAPempty():
-    with open('APs.txt','r') as aps_list:
-        lines = aps_list.readlines()
-        if len(lines) == 0:
+def AP_exist(fileName, bssid):
+    apsList = ET.parse(fileName)
+    aps = apsList.getroot()
+
+    for ap in aps.findall('AccessPoint'):
+        if ap.findtext('BSSID') == bssid:
             return 1
-        else:
-            return 0
+    return 0
 
-def isAPsave(bssid):
-    if isAPempty():
-        return 0
-    else:
-        with open('APs.txt','r') as aps_list:
-            hit=0
-            for line in aps_list:
-                if re.search(bssid,line,re.I):
-                    hit += 1
-            if hit == 0 :
-                return 0
-            else :
-                return 1
+def AP_append(fileName, bssid, ssid, channel, encryption):
+    apsList = ET.parse(fileName)
+    aps = apsList.getroot()
 
-def storeAP(channel,encrypt,bssid,ssid):
-    if isAPsave(bssid):
-        return
-    else:
-        with open('APs.txt','a') as aps_list:
-            ap = str(channel)+','+str(encrypt)+','+str(bssid)+','+str(ssid)+'\n'
-            aps_list.write(ap)
+    if not AP_exist(fileName, bssid):
+        ap = ET.Element("AccessPoint")
+        ap.set("Channel",channel)
+        ap.set("Encryption",encryption)
+        apBssid = ET.SubElement(ap,"BSSID")
+        apBssid.text = bssid
+        apSsid = ET.SubElement(ap,"SSID")
+        apSsid.text = ssid
+        aps.append(ap)
+        apsList.write(fileName)
+
+def APs_show(fileName):
+    apsList = ET.parse(fileName)
+    aps = apsList.getroot()
+
+    ap_table_header = "|"+"Encryption".ljust(10)+" | "+"Channel".ljust(8)+" | "+"BSSID".ljust(18)+" | "+"SSID".ljust(20)+"|"
+    print("_"*len(str(ap_table_header)))
+    print(ap_table_header)
+    for ap in aps.findall('AccessPoint'):
+        enc = str(ap.get('Encryption')).ljust(10)
+        c = str(ap.get('Channel')).ljust(8)
+        bssid = str(ap.findtext('BSSID')).ljust(18)
+        ssid = str(ap.findtext('SSID')).ljust(20)
+        ap_data = "|"+enc+" | "+c+" | "+bssid+" | "+ssid+"|"
+        print(str(ap_data))
+        
+    print("-"*len(str(ap_table_header)))
 
 def clearAP():
-    with open('APs.txt','w') as aps_list:
+    with open(aps_list,'w') as aps_list:
         aps_list.truncate(0)
 
 
@@ -64,7 +82,7 @@ def clearAP():
 #ord() string to integer ex ord('a) will give 97
 def sniffAP(p):
     if ( (p.haslayer(Dot11Beacon))):
-        ssid       = p[Dot11Elt].info
+        ssid       = str(p[Dot11Elt].info, 'utf-8')
         bssid      = p[Dot11].addr3    
         channel    = int( ord(p[Dot11Elt:3].info))
         capability = p.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}\
@@ -75,11 +93,11 @@ def sniffAP(p):
         else: encrypt  = 'N'
 
         # Display discovered AP    
-        if not isAPsave(bssid):
-            print("%02d  %s  %s %s" % (int(channel), encrypt, bssid, ssid))
+        if not AP_exist(aps_list,bssid):
+            print("[AP found]: %02d  %s  %s %s" % (int(channel), encrypt, bssid, ssid))
 
         # Save discovered AP
-        storeAP(channel,encrypt,bssid,ssid)
+        AP_append("APs.xml", str(bssid), str(ssid), str(channel), str(encrypt))
 
 
 
@@ -92,10 +110,10 @@ def channel_hopper():
     up = True
     while True:
         try:
-            print("scanning channel %d" %(channel))
+            print("[Scanning]: channel %d" %(channel))
             os.system("iw dev %s set channel %d" % (interface, channel))
             time.sleep(1)
-            if (channel == 14): up = False
+            if (channel == 13): up = False
             elif (channel == 1): up = True
             if (up == True): channel +=1
             else: channel -=1
@@ -111,31 +129,32 @@ def channel_hopper():
 def signal_handler(signal, frame):
     p.terminate()
     p.join()
+    
+    monitor_down(interface)
 
-    printAP()
-
-
+    os.system('clear')
+    APs_show(aps_list)
+    
     sys.exit(0)
 
 
 #use this for command line variables 
 #for checking the number of command line variables and if they are in right order
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage %s monitor_interface" % sys.argv[0])
-        sys.exit(1)
-
-    #take mon0 as interface given in the fist command line variable
-    interface = sys.argv[1]
-    
-    # Change network adapter to monitor mode
-    monitor_up(interface)
-    
     try:
-        # Print the program header
-        print("-=-=-=-=-=-= scan.py =-=-=-=-=-=-")
-        print("CH ENC BSSID             SSID")
+        if len(sys.argv) != 2:
+            print("[Usage]: %s monitor_interface" % sys.argv[0])
+            sys.exit(1)
 
+        #take mon0 as interface given in the fist command line variable
+        interface = sys.argv[1]
+
+        # Change network adapter to monitor mode
+        interface = monitor_up(interface)
+        print("[Interface]: %s" % interface)
+
+        # Accesspoint list init
+        APs_init(aps_list)
         # Start the channel hopper
         #In multiprocessing, processes are spawned by creating a Process object and then calling its start() method
         p = Process(target = channel_hopper)
@@ -149,7 +168,5 @@ if __name__ == "__main__":
         sniff(iface=interface,prn=sniffAP)
         #inbuit scapy function to start sniffing calls a function which defines the criteria and we need to give the interface
     except KeyboardInterrupt:
-        monitor_down(interface)
-        sys.exit(1)
-
+        print("[Interupt]")
 
